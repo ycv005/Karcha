499442024598b4b53460caec8ec9db214678a7e4
Yash Chandra Verma <ycverma005@gmail.com>
2020-09-09 11:23:18 +0530

    bottomsheet impr

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -32,54 +32,59 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Container(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(labelText: 'Title'),
-              controller: titleController,
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Amount'),
-              controller: amountController,
-              keyboardType: TextInputType.number,
-            ),
-            Container(
-              height: 70,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Text(dateChoosen == null
-                        ? "No Date Choosen"
-                        : DateFormat.yMd().format(dateChoosen)),
-                  ),
-                  FlatButton(
-                    onPressed: _selectDate,
-                    child: Text("Choose a Date"),
-                    textColor: Theme.of(context).primaryColor,
-                  )
-                ],
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Container(
+          padding: EdgeInsets.only(
+            top: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(labelText: 'Title'),
+                controller: titleController,
               ),
-            ),
-            RaisedButton(
-              child: Text('Add Transaction'),
-              color: Theme.of(context).primaryColor,
-              textColor: Theme.of(context).textTheme.button.color,
-              onPressed: () {
-                if (amountController.text.isNotEmpty) {
-                  widget.addTx(
-                    titleController.text,
-                    double.parse(amountController.text),
-                    dateChoosen,
-                  );
-                }
-              },
-            ),
-          ],
+              TextField(
+                decoration: InputDecoration(labelText: 'Amount'),
+                controller: amountController,
+                keyboardType: TextInputType.number,
+              ),
+              Container(
+                height: 70,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(dateChoosen == null
+                          ? "No Date Choosen"
+                          : DateFormat.yMd().format(dateChoosen)),
+                    ),
+                    FlatButton(
+                      onPressed: _selectDate,
+                      child: Text("Choose a Date"),
+                      textColor: Theme.of(context).primaryColor,
+                    )
+                  ],
+                ),
+              ),
+              RaisedButton(
+                child: Text('Add Transaction'),
+                color: Theme.of(context).primaryColor,
+                textColor: Theme.of(context).textTheme.button.color,
+                onPressed: () {
+                  if (amountController.text.isNotEmpty) {
+                    widget.addTx(
+                      titleController.text,
+                      double.parse(amountController.text),
+                      dateChoosen,
+                    );
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
